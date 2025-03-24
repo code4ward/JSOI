25dc25a36cf0e7087ba3e8ce4cec568e8d3e43af
Kev <1835551+code4ward@users.noreply.github.com>
2025-03-23 23:47:40 -0300

    Remove interpolation_file_template.js
    Removed test code from library.

--- rollup.config.js
+++ rollup.config.js
@@ -34,47 +34,6 @@ export default [
             { file: 'dist/interpolation_objects.gs.min.js', name: "JSOI", format: 'iife', sourcemap: true, plugins: [terser()] } // Minimized CommonJS
         ],
         plugins: [resolve(), commonjs()]
-    },
-
-    //
-    // Build for - interpolation_file_template
-    //
-
-    // ES Build
-    {
-        input: 'src/interpolation_file_template.js', // Input file path
-        output: [
-            {
-                file: 'dist/interpolation_file_template.esm', // ESM output (unminified)
-                format: 'esm', // ES Module format
-                sourcemap: true // Optional source map for debugging
-            },
-            {
-                file: 'dist/interpolation_file_template.min.esm', // ESM output (minified)
-                format: 'esm',
-                sourcemap: true, // Optional source map for debugging
-                plugins: [terser()] // Minify the output using Terser
-            }
-        ],
-        plugins: [resolve(), commonjs()]
-    },
-    // CommonJS Build
-    {
-        input: 'src/interpolation_file_template.js', // Input file path
-        output: [
-            {
-                file: 'dist/interpolation_file_template.cjs.js', // CommonJS output (unminified)
-                format: 'cjs', // CommonJS format
-                sourcemap: true // Optional sourcemap for debugging
-            },
-            {
-                file: 'dist/interpolation_file_template.cjs.min.js', // CommonJS output (minified)
-                format: 'cjs',
-                sourcemap: true, // Optional sourcemap for debugging
-                plugins: [terser()] // Minify the output using Terser
-            }
-        ],
-        plugins: [resolve(), commonjs()]
     }
 ];
 
