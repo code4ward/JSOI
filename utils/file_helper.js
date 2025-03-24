b9892e4f5bc9a065b428af9b42e46c076234964e
Kev <1835551+code4ward@users.noreply.github.com>
2025-03-24 16:28:08 -0300

    Better error checking on moving build files

--- utils/file_helper.js
+++ utils/file_helper.js
@@ -3,9 +3,9 @@
 // copy or move a file or move a npm pack to dist folder
 //
 // --------------------------------------------------------------------------------------------------------------------
-import { rename, cp, existsSync, readdirSync, unlinkSync, mkdirSync } from 'fs'
+import { rename, cp, existsSync, readdirSync, unlinkSync, mkdirSync, statSync } from 'fs'
 import * as path from "node:path";
-import {join} from "path";
+import {join, resolve, relative} from "path";
 import {readFile} from "fs/promises";
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -15,6 +15,27 @@ function fail(msg) {
     console.error(msg);
     process.exit(1);
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+//
+// --------------------------------------------------------------------------------------------------------------------
+function isValidPath(filePath, validateFileExists) {
+    const currentDir = resolve('./'); // Resolve current directory
+    const resolvedPath = resolve(filePath); // Resolve input path to an absolute path
+    const relativePath = relative(currentDir, resolvedPath); // Get the relative path from currentDir to resolvedPath
+
+    // Check if the file exists and is a valid file
+    const pathExists = existsSync(resolvedPath);
+    const isFile = pathExists && statSync(resolvedPath).isFile();
+
+    // Ensure the resolved path is within currentDir scope
+    const isWithinCurrentDir = !relativePath.startsWith('..') && !relativePath.startsWith('/') && !path.isAbsolute(relativePath);
+
+    // Return true only if the file exists, is a file, and is within the current directory scope
+    return isWithinCurrentDir && (!validateFileExists || isFile);
+
+}
+
 // --------------------------------------------------------------------------------------------------------------------
 //
 // --------------------------------------------------------------------------------------------------------------------
@@ -104,13 +125,29 @@ async function main()
             const distPackageJsonPath = join("./", 'dist');
             cleanDistFolder(distPackageJsonPath)
         } else if ((cmd === "move") && (process.argv.length === 5)) {
-            const oldFilePath = process.argv[3];
-            const newFilePath = process.argv[4];
-            moveFile(oldFilePath, newFilePath);
+            const sourceFilePath = process.argv[3];
+             if(isValidPath(sourceFilePath, true)) {
+                 const targetFilePath = process.argv[4];
+                 if(isValidPath(targetFilePath, false))
+                    moveFile(sourceFilePath, targetFilePath);
+                 else
+                     fail(`Invalid path: ${targetFilePath}. Please provide a valid path to a target file.`);
+             }
+             else
+                 fail(`Invalid path: ${sourceFilePath}. Please provide a valid path to a source file.`);
+
         } else if ((cmd === "copy") && (process.argv.length === 5)) {
-            const oldFilePath = process.argv[3];
-            const newFilePath = process.argv[4];
-            copyFile(oldFilePath, newFilePath);
+            const sourceFilePath = process.argv[3];
+            if(isValidPath(sourceFilePath, true)) {
+                const targetFilePath = process.argv[4];
+                if(isValidPath(targetFilePath, false))
+                    copyFile(sourceFilePath, targetFilePath);
+                else
+                    fail(`Invalid path: ${targetFilePath}. Please provide a valid path to a target file.`);
+            }
+            else
+                fail(`Invalid path: ${sourceFilePath}. Please provide a valid path to source file.`);
+
         } else
             fail(`Usage: ${path.basename(process.argv[0])} ${path.basename(process.argv[1])} <move | copy> [sourcePath/filename] [targetPath/filename] | ${path.basename(process.argv[0])} ${path.basename(process.argv[1])} MovePkg`)
 
